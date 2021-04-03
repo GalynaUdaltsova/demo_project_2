ce78373c5025b7050e2dadfeddbdd18f4abdd170
Kirill Reuka <reuka.kirill@gmail.com>
2021-04-03 18:56:48 +0300

    Added search test

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class HomePageTest {
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import rozetka.HomePage;
+
+public class HomePageTest extends BaseTest{
+
+    private By titleFirstItem = By.xpath("//span[@class='goods-tile__title'][1]");
+
+    @DataProvider(name = "search")
+    public Object[][] search() {
+        return new Object[][] {
+                { "Apple iPhone 11"},
+                { "Xiaomi Redmi 9A"},
+                { "Samsung Galaxy"},
+                { "Зеркало для макияжа Xiaomi"},
+                { "Набор инструментов"},
+        };
+    }
+
+    @Test(dataProvider = "search")
+    public void search(String item){
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProductByCriteria(item);
+        String titleFirst = driver.findElement(titleFirstItem).getText();
+        Assert.assertTrue(titleFirst.contains(item));
+    }
 }
